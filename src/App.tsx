70bd2bd453fbb711cb8ffe0cdcf7dc088bc60873
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-02-28 02:06:00 +0900

    styling

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent, h } from "preact";
+
+const App: FunctionComponent = () => {
+  return (
+    <div class="container">
+      <textarea class="edit-area" />
+      <div />
+      <pre />
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { h, render } from "preact";
+import App from "./App";
 import "./style.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-render(<h1>Hello, Preact!</h1>, document.getElementById("root")!);
+render(<App />, document.getElementById("root")!);
