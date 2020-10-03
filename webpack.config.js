29d6d780c50a45b02773a57908179ecc69d729b0
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-10-03 12:08:55 +0900

    yarn upgrade -L

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const TerserPlugin = require("terser-webpack-plugin"); // from webpack
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const package = require("./package.json");
 
-/** @type {(env: typeof process.env, argv: { mode?: string }) => import("webpack").Configuration} */
+/** @type {import("webpack").ConfigurationFactory} */
 module.exports = (env, { mode }) => {
   const dev = mode !== "production";
   return {
@@ -58,10 +58,11 @@ module.exports = (env, { mode }) => {
     },
     devtool: dev ? "inline-source-map" : false,
     devServer: {
-      contentBase: "./dist",
       // host: "0.0.0.0", // for debugging on mobile devices
+      historyApiFallback: true,
+      contentBase: "./public", // for static file serving
+      // watchContentBase: true,
       overlay: true,
-      watchContentBase: true,
     },
   };
 };
